0d7b0a386b27b7ed0a0fb57ebe19cc0178cb3ac7
orestesoliveira <orestesoliveira@gmail.com>
2023-06-07 17:26:34 -0300

    Solved order with securityMatcher

--- src/main/java/com/ossabaw/msscdemo/config/SecurityConfig.java
+++ src/main/java/com/ossabaw/msscdemo/config/SecurityConfig.java
@@ -15,19 +15,22 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
     SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
 
         return http
-                .authorizeHttpRequests(auth-> auth.requestMatchers("/api/**").authenticated())
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
                 .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
     @Bean
-    @Order(1)
+    @Order(2)
     SecurityFilterChain h2ConsoleFilterChain(HttpSecurity http) throws Exception {
         return http
+                .securityMatcher(AntPathRequestMatcher.antMatcher("/h2-console/**"))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers((AntPathRequestMatcher.antMatcher("/h2-console/**"))).permitAll();
                 })
